76ef80dcfa2b89f0402e1c4314f67de70040b323
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2022-06-20 20:00:39 +0400

    test

---
+++ less.py
@@ -0,0 +1,13 @@
+# import math
+# print(math.pi)
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+# x = np.arange(1, 10)
+# y = math.sin(x)
+x = 10
+y = 2 * x
+plt.plot(x, y)
+plt.show()
